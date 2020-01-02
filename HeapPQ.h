839d98463aa094df8b2de91a33fe4fa7e6553204
GarrisonTravis <58957083+GarrisonTravis@users.noreply.github.com>
2020-01-02 15:28:09 -0600

    Update HeapPQ.h

--- HeapPQ.h
+++ HeapPQ.h
@@ -19,18 +19,18 @@ public:
 		hPQ.clear();
 	}
 
-    //Returns true if PQ is empty, else return false
-    bool empty() {
-        if (heapSize == 0)
-            return true;
-        else
-            return false;
-    }
+	//Returns true if PQ is empty, else return false
+	bool empty() {
+		if (heapSize == 0)
+			return true;
+		else
+			return false;
+	}
 
-    //Returns the size of the PQ
-    int size() {
-        return heapSize;
-    }
+	//Returns the size of the PQ
+	int size() {
+		return heapSize;
+	}
 
 	//Returns the parent key for the given index
 	pair<int, Type> getParent(int index) {
@@ -49,7 +49,7 @@ public:
 	}
 
 	//Returns the data value of left node
-	Type getLeft(int index) {
+	pair<int, Type> getLeft(int index) {
 		return hPQ[(2 * index) + 1];
 	}
 
@@ -64,13 +64,13 @@ public:
 	}
 
 	//Returns the data value of right node
-	Type getRight(int index) {
+	pair<int, Type> getRight(int index) {
 		return hPQ[(2 * index) + 2];
 	}
 
-    //Pushes a <priority, value> pair onto the heap PQ
-    //Post: The heap PQ will contain the new pair at the correct spot according to its key
-    void push(int priority, Type val) {
+	//Pushes a <priority, value> pair onto the heap PQ
+	//Post: The heap PQ will contain the new pair at the correct spot according to its key
+	void push(int priority, Type val) {
 		heapSize++;
 		int index = heapSize - 1;
 		hPQ.push_back(make_pair(priority, val));	//add new entry to the heap
@@ -85,10 +85,82 @@ public:
 
 			index = getParentIndex(index);		//new index
 		}
-    }
+	}
+
+	//Pops the <priority, value> pair with the highest priority
+	//Post: The heap PQ will no longer contain the pair with highest priority
+	pair<int, Type> pop() {
+		if (empty())
+			throw runtime_error("The Queue is Empty. Max Priority Doesn't Exist");
+
+		//Condition if heap has a size of 1
+		if (heapSize == 1) {
+			pair<int, Type> temp = hPQ.at(0);
+			heapSize--;
+			hPQ.pop_back();
+			return temp;
+		}
+
+		//Swapping the root node with the last node 
+		pair<int, Type> temp = hPQ.at(0);
+		hPQ[0].first = hPQ.at(heapSize - 1).first;
+		hPQ[0].second = hPQ.at(heapSize - 1).second;
+		hPQ[heapSize - 1] = temp;
+
+		//Extract the max value
+		pair<int, Type> max = hPQ.at(heapSize - 1);
+		hPQ.pop_back();
+		heapSize--;
+
+		//Reheaps the heap to satisfy the conditions of a max heap
+		int index = 0;
+		int currentPosition = 0;
+		while (getLeftIndex(index) < heapSize) {
+			//Checks to see if there is a right node and if greater than the left node
+			if (getRightIndex(index) < heapSize && getRight(index).first > getLeft(index).first) {
+				currentPosition = getRightIndex(index);
+			}
+			//Checks to see if there is a right node and if it is equal to the left node
+			else if (getRightIndex(index) < heapSize && getRight(index).first == getLeft(index).first) {
+				//If right and left are equal, look at their val, pick one with lowest value
+				//This is personal design decision. I wanted the lesser value to appear first if left and right node are equal keys
+				if (getRight(index).second < getLeft(index).second) 
+					currentPosition = getRightIndex(index);
+				else
+					currentPosition = getLeftIndex(index);
+			}
+			else {
+				currentPosition = getLeftIndex(index);
+			}
+
+			//Checks if current node is greater than its parent node
+			//If true, then swap the nodes
+			if (hPQ.at(currentPosition).first > hPQ.at(index).first) {
+				temp = hPQ.at(index);
+				hPQ[index] = hPQ.at(currentPosition);
+				hPQ[currentPosition] = temp;
+
+				index = currentPosition;
+			}
+			else
+				break;
+		}
+
+		return max;
+	}
+
+	//Returns the <priority, value> pair with the highest priority
+	//Post: The heap PQ is the same as before
+	pair<int, Type> top() {
+		//Throw exception if the PQ is empty
+		if (empty())
+			throw runtime_error("The Queue is Empty. Max Priority Doesn't Exist");
+
+		return hPQ.at(0);
+	}
 
 	//Outputs the current heap PQ to the screen
-    //Includes the key and value
+	//Includes the key and value
 	void print() {
 		cout << "HeapPQ = [";
 		for (int i = 0; i < size(); i++) {
