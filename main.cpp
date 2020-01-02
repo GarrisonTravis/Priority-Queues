fa852d99c5f7c7e824ce964065cfb441d67e7376
GarrisonTravis <58957083+GarrisonTravis@users.noreply.github.com>
2020-01-02 08:53:35 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,16 +5,28 @@ using namespace std;
 
 int main() {
 
+	cout << "Testing UnsortedPQ: [<key, value>]" << endl;
+	cout << "--------------------------------" << endl;
 	UnsortedPQ <char> u;
 	pair<int, char> p;
 	u.push(1, 'A');
 	u.push(2, 'B');
 	u.push(3, 'C');
 	u.push(2, 'D');
+	u.push(0, 'E');
 	u.print();
-	p = u.top();
-	cout << "Popped: " << "<" << p.first << ", " << p.second << ">" << endl;
-	u.pop();
 	
-	u.print();
+	//Popping every entry in the UnsortedPQ to show that the entries will be sorted by their priority/keys
+	cout << "\nPopping every entry in the UnsortedPQ ..." << endl <<endl ;
+	cout << "By pushing and then popping every entry, the resulting output is sorted by the keys: ";
+	int size = u.size();
+	for (int i = 0; i < size; i++) {
+		p = u.top();
+		if(i == size - 1)
+			cout << "<" << p.first << ", " << p.second << ">";
+		else
+			cout << "<" << p.first << ", " << p.second << ">" << " ";
+		u.pop();
+	}
+	cout << endl;
 }
